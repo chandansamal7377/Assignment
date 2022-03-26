34128d93a1424808a262ca9eedd6bb7ae9e36f80
Chandan Samal <chandansamal9800@gmail.com>
2022-03-26 18:23:32 +0530

    current prob

---
+++ arrayprac.sh
@@ -0,0 +1,9 @@
+declare -a value=( 1 2 3 4 5 6 7 8 9 10 )
+for (( i=1;i<=${#value[@]};i++ ))
+do
+        if [ $(($i%2)) -eq 0 ]
+        then
+           echo "$i is even number"
+
+        fi
+done
